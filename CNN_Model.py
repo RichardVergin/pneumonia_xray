48a22af6491c2cd15035a35a96ba92428b512ac8
Richard Vergin <richard.vergin@icloud.com>
2020-06-19 15:58:01 +0200

    Debugged Shapes

--- CNN_Model.py
+++ CNN_Model.py
@@ -22,10 +22,10 @@ def dataloader(path):
     y_train = np.array(all_data_prepared['y_train'])
     x_train, y_train = shuffle(x_train, y_train, random_state=0) # shuffle before training
     x_test = np.array(all_data_prepared['x_test'])
-    x_test = np.expand_dims(x_train, axis=3)
+    x_test = np.expand_dims(x_test, axis=3) # expand dimensions since grayscale
     y_test = np.array(all_data_prepared['y_test'])
     x_val = np.array(all_data_prepared['x_val'])
-    x_val = np.expand_dims(x_train, axis=3)
+    x_val = np.expand_dims(x_val, axis=3) # expand dimensions since grayscale
     y_val = np.array(all_data_prepared['y_val'])
 
     return x_train, y_train, x_test, y_test, x_val, y_val
